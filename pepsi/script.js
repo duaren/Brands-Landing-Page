45812581ab6710e499359b548133c42a1e6f65a9
duaren <renaudduaren@gmail.com>
2021-06-17 03:18:14 +0100

    add menu icons

--- pepsi/script.js
+++ pepsi/script.js
@@ -4,5 +4,15 @@ function imgSlider (anything){
  
 function changeBgColor (color) {
     const sec = document.querySelector('.sec');
+    const nav = document.querySelector('.nav')
     sec.style.background = color;
+    nav.style.background = color;
+
+}
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const nav = document.querySelector('.nav');
+    menuToggle.classList.toggle('active')
+    nav.classList.toggle('active')
 }
